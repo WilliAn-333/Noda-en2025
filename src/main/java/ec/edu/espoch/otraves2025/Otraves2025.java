b1b8ed70c47ff91a54b13a6230918571c3c55330
WilliAn-333 <www.pegazo23@gmail.com>
2025-01-22 07:05:22 -0500

    nuevo repositorio

---
+++ src/main/java/ec/edu/espoch/otraves2025/Otraves2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.otraves2025;
+
+/**
+ *
+ * @author USER
+ */
+public class Otraves2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
